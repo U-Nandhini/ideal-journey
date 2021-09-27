bcf632ec698ff4ce40776d50e7e05d47a9e7f6f5
U-Nandhini <88302784+U-Nandhini@users.noreply.github.com>
2021-09-27 16:52:45 +0530

    Update ViewUser.js

--- React Task/ViewUser.js
+++ React Task/ViewUser.js
@@ -22,46 +22,17 @@ const User = () => {
     setUser(res.data.data);
   };
   return (
-    <div className="container py-4">
-      <h1 className="display-4">User Id: {id}</h1>
+    <div className="container-user">
+      <h1 className="displayID">User Id: {id}</h1>
       <hr />
     <ul class="text">
-      <li className="list-group-item">User ID : {user.id}</li>
-      <li className="list-group-item">User name:{user.name}</li>
-      <li className="list-group-item">Email ID:{user.email}</li>
-      <li className="list-group-item"> Gender:{user.gender}</li>
-      <li className="list-group-item">Status:{user.status}</li>
+      <li className="listUser">User ID : {user.id}</li>
+      <li className="listUser">User name:{user.name}</li>
+      <li className="listUser">Email ID:{user.email}</li>
+      <li className="listUser"> Gender:{user.gender}</li>
+      <li className="listUser">Status:{user.status}</li>
     </ul>
-      {/* <table className="viewTable">
-        <th>s.no</th>
-        <tr>
-          <td ></td>
-          <td>ID</td>
-          <td>{user.id}</td>
-        </tr>
-        <tr>
-         
-        <td></td>
-         <td>Name</td>
-          <td>{user.name}</td>
-        </tr>
-
-        <tr>
-        <td></td>
-          <td>Gender</td>
-          <td>{user.gender}</td>
-        </tr>
-        <tr>
-        <td></td>
-          <td>Email</td>
-          <td>{user.email}</td>
-        </tr>
-        <tr>
-        <td></td>
-          <td>Status</td>
-          <td>{user.status}</td>
-        </tr>
-      </table> */}
+      
     </div>
   );
 };
